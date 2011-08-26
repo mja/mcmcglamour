68728abfa94a5b20f3c94257036d911e5983e61c
Mark James Adams <m.j.adams-2@sms.ed.ac.uk>
2011-08-26 07:18:53 +0100

    Reshape VCV matrices

---
+++ MCMCglamour.R
@@ -0,0 +1,33 @@
+
+match.length <- function(x) attr(x, 'match.length')
+
+match.random <- function(x) {
+
+        r <- regexpr("\\w+$", x, perl=TRUE)
+        random <- substr(x, r[1], r[1] + match.length(r)[1] - 1)
+
+        return(random)
+ }
+
+ match.response <- function(x) {
+
+         r <- regexpr("^[a-z]+", x, perl=TRUE)
+         response <- substr(x, r[1], r[1] + match.length(r)[1] - 1)
+
+         return(response)
+ }
+
+# Turn a two dimensional nitt × (v × p × p) matrix of (co)variances
+# into a nitt × p × p × v dimensional array
+reshape.VCV <- function(VCV) {
+
+  # extract random effect and response terms from the VCV dimension names
+  random <- unique(sapply(dimnames(VCV)[[2]], match.random))
+  response <- unique(sapply(dimnames(VCV)[[2]], match.response))
+
+  nitt <- dim(VCV)[1] # number of iterations
+  V <- array(as.vector(VCV), dim=c(nitt, length(response), length(response), length(random)), dimnames=list(itt=as.character(1:nitt), trait=response, trait=response, V=random))
+
+  return(V)
+}
+
